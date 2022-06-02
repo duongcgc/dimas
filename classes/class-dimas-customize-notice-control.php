93a16f774e5853b9aa8e6e9e8e9e4c2174b7b906
Johnathan Nguyen <duongcgc@gmail.com>
2022-06-02 14:57:51 +0700

    Init

---
+++ classes/class-dimas-customize-notice-control.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Customize API: Dimas_Customize_Notice_Control class
+ *
+ * @package WordPress
+ * @subpackage Dimas
+ * @since Dimas 1.0
+ */
+
+/**
+ * Customize Notice Control class.
+ *
+ * @since Dimas 1.0
+ *
+ * @see WP_Customize_Control
+ */
+class Dimas_Customize_Notice_Control extends WP_Customize_Control {
+	/**
+	 * The control type.
+	 *
+	 * @since Dimas 1.0
+	 *
+	 * @var string
+	 */
+	public $type = 'dimas-notice';
+
+	/**
+	 * Renders the control content.
+	 *
+	 * This simply prints the notice we need.
+	 *
+	 * @since Dimas 1.0
+	 *
+	 * @return void
+	 */
+	public function render_content() {
+		?>
+		<div class="notice notice-warning">
+			<p><?php esc_html_e( 'To access the Dark Mode settings, select a light background color.', 'twentytwentyone' ); ?></p>
+			<p><a href="<?php echo esc_url( __( 'https://wordpress.org/support/article/dimas/#dark-mode-support', 'twentytwentyone' ) ); ?>">
+				<?php esc_html_e( 'Learn more about Dark Mode.', 'twentytwentyone' ); ?>
+			</a></p>
+		</div>
+		<?php
+	}
+}
