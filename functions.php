5c645a08f317bb4ea570dd99a789a6b973985f02
Johnathan Nguyen <duongcgc@gmail.com>
2022-06-07 17:41:10 +0700

    u

--- functions.php
+++ functions.php
@@ -20,6 +20,14 @@ if ( ! defined( 'DIMAS_INC_DIR' ) ) {
 	define( 'DIMAS_INC_DIR', get_template_directory() . '/inc' );
 }
 
+if ( ! defined( 'DIMAS_INC_URI' ) ) {
+	define( 'DIMAS_INC_URI', get_template_directory_uri() . '/inc' );
+}
+
+if ( ! defined( 'DIMAS_ASSETS_URI' ) ) {
+	define( 'DIMAS_ASSETS_URI', get_template_directory_uri() . '/assets' );
+}
+
 if ( ! defined( 'DIMAS_ADDONS_DIR' ) ) {
 	define( 'DIMAS_ADDONS_DIR', DIMAS_INC_DIR . '/addons' );
 }
@@ -28,16 +36,23 @@ if ( ! defined( 'DIMAS_CORE_DIR' ) ) {
 	define( 'DIMAS_CORE_DIR', DIMAS_INC_DIR . '/core' );
 }
 
-if ( ! defined( 'DIMAS_ASSETS_URI' ) ) {
-	define( 'DIMAS_ASSETS_URI', get_template_directory_uri() . '/assets' );
+if ( ! defined( 'DIMAS_ADDONS_URL' ) ) {
+	define( 'DIMAS_ADDONS_URI', DIMAS_INC_URI . '/addons' );
+}
+
+if ( ! defined( 'DIMAS_CORE_URI' ) ) {
+	define( 'DIMAS_CORE_URI', DIMAS_INC_URI . '/core' );
 }
 
 if ( ! defined( 'DIMAS_JS_URI' ) ) {
 	define( 'DIMAS_JS_URI', DIMAS_ASSETS_URI . '/js' );
 }
 
+if ( ! defined( 'DIMAS_CSS_URI' ) ) {
+	define( 'DIMAS_CSS_URI', DIMAS_ASSETS_URI . '/js' );
+}
 
-// Setup Theme.
+// Setup Theme =================.
 require DIMAS_INC_DIR . '/class-dimas-setup.php';
 \Dimas\Theme_Setup::instance();
 
@@ -85,3 +100,7 @@ require DIMAS_INC_DIR . '/block-styles.php';
 // Dark Mode.
 require_once DIMAS_INC_DIR . '/class-dimas-dark-mode.php';
 new \Dimas\Dark_Mode();
+
+// Loading Addons
+require_once DIMAS_ADDONS_DIR . '/class-dimas-addons-plugin.php';
+\Dimas\Addons::instance();
